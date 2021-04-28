ab918342895479942bece45afe28dca415d6eeb3
Edith <edith.bautistag@gmail.com>
2021-04-27 22:56:10 -0500

    Se trato de solucionar e problema de llamar varias veces al set para modificar los datos del objeto

---
+++ src/LunchOrderTeleDemo.java
@@ -0,0 +1,9 @@
+public class LunchOrderTeleDemo {
+    public static void main(String[] args) {
+        LunchOrderTele lunchOrderTele= new LunchOrderTele("Wheat", "Letuce", "Mustard", "Ham");
+        System.out.println(lunchOrderTele.getBread());
+        System.out.println(lunchOrderTele.getCondiments());
+        System.out.println(lunchOrderTele.getDressing());
+        System.out.println(lunchOrderTele.getMeat());
+    }
+}
